5f65735a7faefc88e888a4d7773d2af6d1094ee3
Glarity <tolson519@gmail.com>
2019-06-28 22:31:07 -0700

    Fixed drawing bug, right click draws in blue

--- grid.py
+++ grid.py
@@ -1,11 +1,12 @@
 class Grid:
-    def __init__(self, size, canvas):
+    def __init__(self, size, canvas, canvas_width, canvas_height):
         self.size = size
         self.canvas = canvas
+        self.canvas_width = canvas_width
+        self.canvas_height = canvas_height
         self.draw()
 
     def increase_size(self):
-        print("increase_size")
         self.size += 5
         self.draw()
 
@@ -16,9 +17,8 @@ class Grid:
 
     def draw(self):
         self.canvas.delete("all")
-        grid_width = round(self.canvas.winfo_width() / self.size)
-        grid_height = round(self.canvas.winfo_height() / self.size)
-        print("{}, {}".format(grid_width, grid_height))
+        grid_width = round(self.canvas_width / self.size)
+        grid_height = round(self.canvas_height / self.size)
         for i in range(grid_width):
             for j in range(grid_height):
                 self.canvas.create_rectangle(self.size * i + 1, self.size * j + 1, self.size * (i + 1) + 1, self.size * (j + 1) + 1, tags="grid_sqaure_" + str(i) + "_" + str(j))
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ HEIGHT = 600
 WIDTH = 800
 
 root = tk.Tk()
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-main_grid = Grid(15, canvas, )
-
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="white")
+main_grid = Grid(15, canvas, WIDTH, HEIGHT)
 
 decrease_size_button = tk.Button(root, text="Decrease Grid Size", command=main_grid.decrease_size)
 increase_size_button = tk.Button(root, text="Increase Grid Size", command=main_grid.increase_size)
@@ -16,12 +15,15 @@ increase_size_button = tk.Button(root, text="Increase Grid Size", command=main_g
 def mouse_click(event):
     grid_x = int(event.x / main_grid.size)
     grid_y = int(event.y / main_grid.size)
-    print(grid_x, grid_y)
-    canvas.itemconfig("grid_sqaure_" + str(grid_x) + "_" + str(grid_y), fill='red')
+    if (event.num == 1):
+        fill_color = "red"
+    elif (event.num == 3):
+        fill_color = "blue"
+    canvas.itemconfig("grid_sqaure_" + str(grid_x) + "_" + str(grid_y), fill=fill_color)
 
 
 canvas.bind("<Button-1>", mouse_click)
-
+canvas.bind("<Button-3>", mouse_click)
 canvas.pack()
 decrease_size_button.pack()
 increase_size_button.pack()
