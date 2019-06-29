1be2809f2b4585a5a8c536c0869790b87557c0bd
Glarity <tolson519@gmail.com>
2019-06-29 10:52:03 -0700

    Grid is now a 2D dictionary which allows zooming without data loss

--- grid.py
+++ grid.py
@@ -1,24 +1,30 @@
 class Grid:
-    def __init__(self, size, canvas, canvas_width, canvas_height):
+    def __init__(self, scale, size, canvas, canvas_width, canvas_height):
+        self.scale = scale
         self.size = size
         self.canvas = canvas
         self.canvas_width = canvas_width
         self.canvas_height = canvas_height
+        self.array = {(i, j): "white" for i in range(self.size) for j in range(self.size)}
         self.draw()
 
-    def increase_size(self):
-        self.size += 5
+    def increase_scale(self):
+        self.scale += 5
         self.draw()
 
-    def decrease_size(self):
-        if self.size > 10:
-            self.size -= 5
+    def decrease_scale(self):
+        if self.scale > 10:
+            self.scale -= 5
             self.draw()
+
+    def change_color(self, x, y, color):
+        self.array[x, y] = color
+        self.draw()
 
     def draw(self):
         self.canvas.delete("all")
-        grid_width = round(self.canvas_width / self.size)
-        grid_height = round(self.canvas_height / self.size)
-        for i in range(grid_width):
-            for j in range(grid_height):
-                self.canvas.create_rectangle(self.size * i + 1, self.size * j + 1, self.size * (i + 1) + 1, self.size * (j + 1) + 1, tags="grid_sqaure_" + str(i) + "_" + str(j))
+        grid_width = round(self.canvas_width / self.scale)
+        grid_height = round(self.canvas_height / self.scale)
+        for i in range(self.size):
+            for j in range(self.size):
+                self.canvas.create_rectangle(self.scale * i + 1, self.scale * j + 1, self.scale * (i + 1) + 1, self.scale * (j + 1) + 1, tags="grid_sqaure_" + str(i) + "_" + str(j), fill=self.array[i, j])
--- main.py
+++ main.py
@@ -6,25 +6,25 @@ WIDTH = 800
 
 root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="white")
-main_grid = Grid(15, canvas, WIDTH, HEIGHT)
+main_grid = Grid(15, 100, canvas, WIDTH, HEIGHT)
 
-decrease_size_button = tk.Button(root, text="Decrease Grid Size", command=main_grid.decrease_size)
-increase_size_button = tk.Button(root, text="Increase Grid Size", command=main_grid.increase_size)
+decrease_scale_button = tk.Button(root, text="Decrease Grid Scale", command=main_grid.decrease_scale)
+increase_scale_button = tk.Button(root, text="Increase Grid Scale", command=main_grid.increase_scale)
 
 
 def mouse_click(event):
-    grid_x = int(event.x / main_grid.size)
-    grid_y = int(event.y / main_grid.size)
+    grid_x = int(event.x / main_grid.scale)
+    grid_y = int(event.y / main_grid.scale)
     if (event.num == 1):
         fill_color = "red"
     elif (event.num == 3):
         fill_color = "blue"
-    canvas.itemconfig("grid_sqaure_" + str(grid_x) + "_" + str(grid_y), fill=fill_color)
+    main_grid.change_color(grid_x, grid_y, fill_color)
 
 
 canvas.bind("<Button-1>", mouse_click)
 canvas.bind("<Button-3>", mouse_click)
 canvas.pack()
-decrease_size_button.pack()
-increase_size_button.pack()
+decrease_scale_button.pack()
+increase_scale_button.pack()
 root.mainloop()
