3bad55113d97c20f40b66e88ba3925ea100a2c56
Glarity <tolson519@gmail.com>
2019-06-29 11:17:39 -0700

    Draw multiple squares by holding down mouse buttons

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ decrease_scale_button = tk.Button(root, text="Decrease Grid Scale", command=main
 increase_scale_button = tk.Button(root, text="Increase Grid Scale", command=main_grid.increase_scale)
 
 
-def mouse_click(event):
+def drawing(event):
     grid_x = int(event.x / main_grid.scale)
     grid_y = int(event.y / main_grid.scale)
-    if (event.num == 1):
+    fill_color = ""
+    if (event.state == 256):
         fill_color = "red"
-    elif (event.num == 3):
+    elif (event.state == 1024):
         fill_color = "blue"
     main_grid.change_color(grid_x, grid_y, fill_color)
 
 
-canvas.bind("<Button-1>", mouse_click)
-canvas.bind("<Button-3>", mouse_click)
+canvas.bind("<B1-Motion>", drawing)
+canvas.bind("<B3-Motion>", drawing)
 canvas.pack()
 decrease_scale_button.pack()
 increase_scale_button.pack()
